376da9b292df0ac5ff2864a44de6fdbabc830529
83315 <alshaibaninada@gmail.com>
2024-04-18 15:13:03 +0300

    Add project files.

---
+++ WebApplication4/Controllers/BlogController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class BlogController : Controller
+    {
+
+        public BlogController()
+        {
+        }
+        public IActionResult Index()
+        {
+            BlogContext blogContext = new BlogContext();
+            var blogs = blogContext.Blogs.ToList();
+            return View(blogs);
+        }
+    }
+}
---
+++ WebApplication4/Models/BlogContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication4.Models
+{
+    public class BlogContext : DbContext
+    {
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Blog>().HasData(
+                new Blog
+                {
+                    Id = 1,
+                    Name = "Food Blog",
+                    URL = "https://www.myfood.eat"
+                }
+            );
+        }
+        public DbSet<Blog> Blogs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           
+                optionsBuilder.UseSqlite("Data Source=blog.db");
+               
+        }
+    }
+}
